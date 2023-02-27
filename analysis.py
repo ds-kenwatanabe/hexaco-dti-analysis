c71e6bafb4af68083cd75f3b00126f5763b90f17
chris <ollhoffchristian@gmail.com>
2023-02-27 08:46:49 -0300

    Identation correction and commenting lines

--- analysis.py
+++ analysis.py
@@ -833,8 +833,8 @@ freq_table = freq_table.round(1)
 freq_table = freq_table.astype(str).applymap(lambda x: x + '%')
 # print(freq_table)
 
-table_hexaco = pd.pivot_table(df, index=['course'], values=['H',
-                                                            'E', 'X', 'A', 'C', 'O'],
+table_hexaco = pd.pivot_table(df, index=['course'], values=[
+                              'H', 'E', 'X', 'A', 'C', 'O'],
                               aggfunc={'H': [np.mean, np.std],
                                        'E': [np.mean, np.std],
                                        'X': [np.mean, np.std],
@@ -856,9 +856,9 @@ tests_C = ttest(df['C'][df['sex'] == 'Female'], group1_name='Female',
                 group2=df['H'][df['sex'] == 'Male'], group2_name='Male')
 tests_O = ttest(df['O'][df['sex'] == 'Female'], group1_name='Female',
                 group2=df['H'][df['sex'] == 'Male'], group2_name='Male')
-print(tests_H)
-print(tests_E)
-print(tests_X)
-print(tests_A)
-print(tests_C)
-print(tests_O)
+# print(tests_H)
+# print(tests_E)
+# print(tests_X)
+# print(tests_A)
+# print(tests_C)
+# print(tests_O)
