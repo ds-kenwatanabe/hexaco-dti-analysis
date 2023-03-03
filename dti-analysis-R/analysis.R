e92506d86e9df11e97f17bbe8b1d14275596d8ef
chris <ollhoffchristian@gmail.com>
2023-03-03 13:10:46 -0300

    updated packages and script

--- dti-analysis-R/analysis.R
+++ dti-analysis-R/analysis.R
@@ -13,6 +13,26 @@ summary(fit, fit.measures=TRUE)
 
 polychoric(df)
 
+# Parallel analysis
+scree(df)
+fa.parallel(df, nfactors=4, fm='ml', fa='fa', 
+            main='Parallel Analysis Scree Plots', 
+            n.iter=1000)
+
+# VSS, MAP
+vss(df, n=4, fm="mle", rotate="oblimin")
+
+
+# 2 new factors
+FACTORS2.model <- ' factor_1 =~ dti_1 + dti_2 + dti_3 + 
+              dti_4 + dti_5 + dti_6 + dti_7 + dti_8 + 
+              dti_9 + dti_10
+              factor_2 =~ dti_11 + dti_12 + dti_13 + 
+              dti_14 + dti_15'
+
+fit2 <- cfa(FACTORS2.model, data = df)
+summary(fit2, fit.measures=TRUE)
+
 # 3 new factors
 FACTORS3.model <- ' factor_1 =~ dti_1 + dti_2 + dti_3 + 
               dti_4 + dti_5
--- dti-analysis-R/packages.R
+++ dti-analysis-R/packages.R
@@ -2,5 +2,7 @@
 install.packages('lavaan', dependencies = TRUE)
 install.packages('psych', type = 'source', repos = 
                    'http://personality-project.org/r/')
+install.packages(("GPArotation"))
 library('lavaan')
 library('psych')
+library('GPArotation')
