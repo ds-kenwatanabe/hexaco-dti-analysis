1e2ae60cb5d6c2a85e78f922c473539031b0fc8c
chris <ollhoffchristian@gmail.com>
2023-03-13 13:48:52 -0300

    updated R files

--- dti-analysis-R/packages.R
+++ dti-analysis-R/packages.R
@@ -1,8 +1,11 @@
 # Packages
-install.packages('lavaan', dependencies = TRUE)
+install.packages('lavaan', dependencies = TRUE, INSTALL_opts = '--no-lock')
 install.packages('psych', type = 'source', repos = 
                    'http://personality-project.org/r/')
 install.packages(("GPArotation"))
+install.packages('semTools', INSTALL_opts = '--no-lock')
+
 library('lavaan')
 library('psych')
 library('GPArotation')
+library('semTools')
